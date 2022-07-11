868a171327c88b8bdf7778eeb9edec117c4481e6
newvaldas <vrldas11@gmail.com>
2022-07-11 21:59:26 +0300

    Simplified game time function

--- speed_main_GUI.py
+++ speed_main_GUI.py
@@ -22,42 +22,13 @@ def new_game() -> None:
     
 
     def game_time() -> None:
-        global time, score, missed_word, count_words
+        global time
     
-        if(count_words <= 10): # jei count_words maziau uz 10 atnaujina laika
-            time += 1
-            clock.configure(text=time)
-            clock.after(1000, game_time)
+        time += 1
+        clock.configure(text=time)
+        clock.after(1000, game_time)
         
-        else:
-            result = Label(win, text="Test results:", font=("Arial", 24, "bold"), fg="dark blue")
-            result.place(x=230, y=150)
-            result.configure(text="Time taken = {} \n Your score = {} \n Missed words = {}"
-                            .format(time, score, count_words-score))
-            
-            # exit_btn = Button(win, text="Exit", bg="azure", fg="green", width=15, height=2, command=win.destroy)
-            # exit_btn['font'] = font.Font(size=20)
-            # exit_btn.place(x=330, y=350)
-            
-            # restart_btn = Button(win, text="Restart", bg="azure", fg="red", width=15, height=2, command=new_game)
-            # restart_btn['font'] = font.Font(size=20)
-            # restart_btn.place(x=130, y=350)
-        
-
-            score = 0
-            missed_word = 0
-            time = 0
-            count_words = 0
-
-            next_word.destroy()
-            user_input.destroy()
-            score_label.destroy()
-            score_board.destroy()
-            clock_label.destroy()
-            clock.destroy()    
-
-
-    
+       
     def start_game(event):
         logging.info(f"User input word: {user_input.get()} ") # loginimas
         global score, missed_word, count_words
@@ -72,10 +43,31 @@ def new_game() -> None:
             score_board.configure(text=score)
             
         count_words +=1 # bendra zodziu ivedimas
-        if (count_words <= 10):
+        if (count_words <10):
             random.shuffle(words)
             next_word.configure(text=words[0]) #pirmas words listo elementas "next_word" lable
             user_input.delete(0, END)
+            
+        else:
+            result = Label(win, text="Test results:", font=("Arial", 24, "bold"), fg="dark blue")
+            result.place(x=230, y=150)
+            result.configure(text="Time taken = {} \n Your score = {} \n Missed words = {}"
+                            .format(time, score, count_words-score))
+            
+            exit_btn = Button(win, text="Exit", bg="azure", fg="green", width=15, height=2, command=win.destroy)
+            exit_btn['font'] = font.Font(size=20)
+            exit_btn.place(x=330, y=350)
+            
+            restart_btn = Button(win, text="Restart", bg="azure", fg="red", width=15, height=2, command=new_game)
+            restart_btn['font'] = font.Font(size=20)
+            restart_btn.place(x=130, y=350)
+        
+            next_word.destroy()
+            user_input.destroy()
+            score_label.destroy()
+            score_board.destroy()
+            clock_label.destroy()
+            clock.destroy() 
     
   
 
@@ -102,17 +94,13 @@ def new_game() -> None:
     user_input.place(x=150, y=330)
     user_input.focus_set()
     
-    
-    
     win.bind("<Return>", start_game)
     win.mainloop()
 
- 
-    
 win = Tk()  # pagarindinis langas su start ir exit mygtukais
 win.geometry("700x500")
 win.title("Speed Typing Test")
-new_image = Image.open("/Users/valkri/Documents/kursinis/kursinis/pictures/picture1.jpg") # background
+new_image = Image.open("./pictures/picture1.jpg") # background
 new_bg = ImageTk.PhotoImage(new_image)
 
 lbl = Label(win,image=new_bg)
@@ -131,7 +119,6 @@ start_btn.place(x=230, y=240)
 exit_btn = Button(win, text="Exit", bg="azure", fg="green", width=15, height=2, command=win.destroy)
 exit_btn['font'] = font.Font(size=20)
 exit_btn.place(x=230, y=340)
-
 
 win.mainloop()
 
